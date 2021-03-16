5b7b5d80b76f719e9076a9b040670a959339f8cd
Jarek Matwijewicz <alterro@gmail.com>
2021-03-16 20:50:03 +0100

    change to render, add initActions

--- src/js/script.js
+++ src/js/script.js
@@ -8,14 +8,21 @@
     },
     container: {
       bookList: '.books-list',
+      bookImage: '.book__image'
     },
+    book: {
+      id: 'data-id',
+      f: 'favorite',
+    }
   };
 
   const templates = {
     bookTemp: Handlebars.compile(document.querySelector(select.template.book).innerHTML),
   };
 
-  function renderHTMLBook(){
+  const favoriteBooks = [];
+
+  function render(){
     for (let book of dataSource.books){
       const generatedHTML = templates.bookTemp(book);
       /* [DONE] create element using utils.createElementFromHTML */
@@ -24,8 +31,23 @@
       const bookListContainer = document.querySelector(select.container.bookList);
       /* [DONE] add element to menu */
       bookListContainer.appendChild(DOMelement);
-      console.log(DOMelement);
     }
   }
-  renderHTMLBook();
+
+
+  function initActions(){
+    const linksWithImages = document.querySelectorAll(select.container.bookImage);
+    for (let linkWithImage of linksWithImages){
+      linkWithImage.addEventListener('dblclick',function(){
+        const attributeOfLink = linkWithImage.getAttribute(select.book.id);        
+        favoriteBooks.push(attributeOfLink);
+        console.log(favoriteBooks);
+        linkWithImage.classList.add(select.book.f);
+      });
+    }
+  }
+
+
+  render();
+  initActions();
 }
